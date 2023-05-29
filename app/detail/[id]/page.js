0fc3f57f291b43fb965480d4377b11d0a72d098d
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-05-29 18:43:06 +0900

    get props params2

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 export default async function Detail(props){
     const client=await connectDB;
   const db=client.db("forum")
-  let result=await db.collection('post').findOne({ _id:new ObjectId("6472c81f2acb5b5b325d7ba5")})
+  let result=await db.collection('post').findOne({ _id:new ObjectId(props.params.id)})
   console.log(props)
     return(
         <div>
--- app/list/page.js
+++ app/list/page.js
@@ -1,21 +1,24 @@
 import { connectDB } from "@/util/database";
+import {Link} from "next/link";
+import DetailLink from "./DetailLink";
+
 export default async function List() {
 
     const client=await connectDB;
     const db=client.db("forum")
     let result=await db.collection('post').find().toArray()
-
-    let data
+    
     return (
       <div className="list-bg">
-        {result.map(function(a,i){
-            return(
-                <div className="list-item">
-                <h4>{result[i].title}</h4>
-                <p>{result[i].content}</p>
-              </div>  
-            )
-        })}
+        {
+          result.map((a, i)=>
+            <div className="list-item">
+              <h4>{a.title}</h4>
+              <DetailLink></DetailLink>
+              <p>1월 1일</p>
+            </div>
+          )
+        }
       </div>
     )
-  } 
+  }
