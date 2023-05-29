7480e5b991112a0892ad43536d3c107c8bfb9502
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-05-29 18:06:32 +0900

    get props params

---
+++ app/detail/id/page.js
@@ -0,0 +1,17 @@
+import {connectDB} from "@/util/database"
+import { ObjectId } from "mongodb";
+
+export default async function Detail(props){
+    const client=await connectDB;
+  const db=client.db("forum")
+  let result=await db.collection('post').findOne({ _id:new ObjectId("6472c81f2acb5b5b325d7ba5")})
+  console.log(props)
+    return(
+        <div>
+            <h4>상세페이지</h4>
+            <h4>{result.title}</h4>
+            <p>{result.content}</p>
+        </div>
+    )
+}
+//props는 파일명에[] 자리에 입력한 값을 가져옴
