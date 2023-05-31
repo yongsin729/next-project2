b7cd3e449e813decff4eabe77c4222de1533e9ad
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-05-31 23:01:03 +0900

    useRouter

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -2,11 +2,12 @@ import { connectDB } from "@/util/database"
 import { ObjectId } from "mongodb";
 
 export default async function Detail(props){
-const client=await connectDB;
+// const client=await connectDB;
 let db = (await connectDB).db('forum');
-let result = await db.collection('post').find().toArray();
+let result = await db.collection('post').findOne({_id:new ObjectId(props.params.id)})
     return(
         <div>
+            {console.log(result.title)}
             <h4>상세페이지</h4>
             <h4>{result.title}</h4>
             <p>{result.content}</p>
