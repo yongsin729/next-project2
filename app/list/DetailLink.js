514c56085b15f50e9cf603132227f66abde3fb9c
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-06-03 16:08:29 +0900

    useRouter2

--- app/list/DetailLink.js
+++ app/list/DetailLink.js
@@ -1,9 +1,12 @@
 'use client'
 
+import { useRouter } from "next/navigation"
+
 export default function DetailLink(){
+    let router=useRouter()
     return(
         <button onClick={()=>{
-            
+            router.push('/')
         }}>버튼</button>
     )
 }
--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,6 @@
 import { connectDB } from "@/util/database.js"
 import Link from "next/link";
-
+import DetailLink  from "./DetailLink";
 export default async function List() {
   let db = (await connectDB).db('forum')
   let result = await db.collection('post').find().toArray()
@@ -10,6 +10,7 @@ export default async function List() {
         result.map((a, i)=>
           <div className="list-item">
             <Link href={'/detail/' + result[i]._id}><h4>{a.title}</h4></Link>
+            <DetailLink></DetailLink>
             <p>1월 1일</p>
           </div>
         )
