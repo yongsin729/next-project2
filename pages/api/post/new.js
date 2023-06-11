3a88ceacb9b01c55f85eed383da7b516df30b95d
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-06-11 16:22:49 +0900

    post final

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,5 +1,14 @@
+import { connectDB } from "@/util/database"
+
 export default async function Handler(요청,응답){
-    
-        응답.status(200).json(요청.body) 
-    
+      
+      if(요청.body.title==""){
+            return 응답.status(500).json('제목안씀');
+      }
+            const db = (await connectDB).db('forum')
+      let result = await db.collection('post').insertOne(요청.body)
+      응답.redirect(302,'/list')
+      
+              
+      
   }
